0ebbdfc7731f9330148edfdd039680ea1c8064dc
meron14725 <meron14725@yahoo.co.jp>
2026-01-11 01:50:23 +0900

    add: サイドバーコンポーネント作成

--- src/components/sidebar/SidebarOptions.js
+++ src/components/sidebar/SidebarOptions.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./SidebarOptions.css"
 
 function SidebarOptions({ text, Icon }) {
   return (
-    <div>
+    <div className="sidebarOption">
       <Icon />
       <h2>{text}</h2>
     </div>
